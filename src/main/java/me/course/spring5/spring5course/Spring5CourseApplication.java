fc4781a65583d0fb46d7a8ed9a4433f687baeaea
Danny <danny.lucas@live.nl>
2021-04-13 15:38:11 +0200

    initial commit

---
+++ src/main/java/me/course/spring5/spring5course/Spring5CourseApplication.java
@@ -0,0 +1,13 @@
+package me.course.spring5.spring5course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5CourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5CourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/course/spring5/spring5course/Spring5CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package me.course.spring5.spring5course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
